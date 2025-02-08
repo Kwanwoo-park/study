687ef96ef8c59e35a68a8e1189edb696ed231f6e
akakslslzz@naver.com <akakslslzz@naver.com>
2025-02-08 23:47:38 +0900

    Board title Search Update

--- src/main/resources/static/js/board/all.js
+++ src/main/resources/static/js/board/all.js
@@ -1,11 +1,17 @@
 const title = document.getElementById('title');
+const clear = document.querySelector(".btnClear")
 
 if (title) {
     title.addEventListener('keydown', (event) => {
         if (event.key == 'Enter') {
-            let temp = title.value;
-            title.value = null;
-            location.href = "/board/all?title=" + temp;
+            //console.log(title.value)
+            location.href = "/board/all?title=" + title.value;
         }
+    })
+}
+
+if (clear) {
+    clear.addEventListener('click', (event) => {
+        title.value = null
     })
 }
