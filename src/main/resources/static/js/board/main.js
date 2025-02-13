237e0208307cfb25ff9612093d1241afa412d33d
akakslslzz@naver.com <akakslslzz@naver.com>
2025-02-13 21:27:56 +0900

    Insta main UI develop

--- src/main/resources/static/js/board/main.js
+++ src/main/resources/static/js/board/main.js
@@ -1,13 +1,31 @@
 function fnLeft(listId, ImageArr) {
-    console.log(ImageArr);
+    const main_image = document.getElementById('main_img' + listId);
+    const img_id = document.getElementById('img' + listId);
+    const left_arrow = document.getElementById('left' + listId);
+    const right_arrow = document.getElementById('right' + listId);
+
+    main_image.src = "/img/" + ImageArr[parseInt(img_id.value) - 1].imgSrc;
+    img_id.value = parseInt(img_id.value) - 1;
+
+    if (right_arrow.style.display === 'none')
+        right_arrow.style.display = 'flex'
+
+    if (parseInt(img_id.value) == 0)
+        left_arrow.style.display = 'none'
 }
 
 function fnRight(listId, ImageArr) {
-    console.log(ImageArr);
     const main_image = document.getElementById('main_img' + listId);
     const img_id = document.getElementById('img' + listId);
-    console.log(img_id.value);
-    console.log(ImageArr[parseInt(img_id.value )+ 1])
-    main_image.src = "/img/" + ImageArr[parseInt(img_id.value )+ 1].imgSrc;
-    img_id.value = parseInt(img_id.value )+ 1;
+    const left_arrow = document.getElementById('left' + listId);
+    const right_arrow = document.getElementById('right' + listId);
+
+    main_image.src = "/img/" + ImageArr[parseInt(img_id.value)+ 1].imgSrc;
+    img_id.value = parseInt(img_id.value)+ 1;
+
+    if (left_arrow.style.display === 'none')
+        left_arrow.style.display = 'flex'
+
+    if (parseInt(img_id.value) == ImageArr.length-1)
+        right_arrow.style.display = 'none'
 }
