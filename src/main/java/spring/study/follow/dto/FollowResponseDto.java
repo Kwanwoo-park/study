0e7df7daedbf64f281cff08cb1b113530f5ea50d
Kwanwoo-park <akakslslzz@naver.com>
2026-04-11 23:51:47 +0900

    [Fix] Follow UI

--- src/main/java/spring/study/follow/dto/FollowResponseDto.java
+++ src/main/java/spring/study/follow/dto/FollowResponseDto.java
@@ -6,10 +6,12 @@ import spring.study.member.entity.Member;
 
 @Getter
 public class FollowResponseDto {
+    private Long id;
     private Member follower;
     private Member following;
 
     public FollowResponseDto(Follow entity) {
+        this.id = entity.getId();
         this.follower = entity.getFollower();
         this.following = entity.getFollowing();
     }
@@ -17,6 +19,7 @@ public class FollowResponseDto {
     @Override
     public String toString() {
         return "FollowResponseDto{" +
+                "id=" + id +
                 "follower=" + follower +
                 ", following=" + following +
                 '}';
